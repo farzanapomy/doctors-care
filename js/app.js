cb65cc344304e57ccfc8452a23c8d40707ccc6be
farzanapomy <farzanapomy56@gmail.com>
2022-03-02 03:25:36 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('.second-text').css({ 'margin- top': '1000px', 'padding': '10px' });
+
+// $('.third-section').css("background-image", "url(lib/images/path.png)");
+// $('.third-section').css("background-repeat", "no-repeat");
+// $('.third-section').css("width", "100vw");
+// $('.third-section').css("height", "210vh");
